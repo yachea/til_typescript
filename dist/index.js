2f55c4806817606a5d109d15dc6b9b87a6f10499
yachea <dev.yachea@gmail.com>
2025-06-18 16:08:33 +0900

    [docs] 타입스크립트 기본자료형

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-import { hello } from "./hello";
-hello();
+const strVar = "string";
+const numVar = 0;
+const boolVar = true;
+const symVar = Symbol("age");
+const nullVar = null;
+const undeVar = undefined;
+const bigIntVar = BigInt(99999999);
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-const a = "안녕";
+let neverVar: never = 100;
+let neverVar2: never = true;
+let neverVar3: never = undefined;
+let neverVar3: never = null;
+
+// 전달시 오류발생
+let testNum: number = neverVar;
