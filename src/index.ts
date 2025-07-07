b99b308144b413d56e2d45eaf4eec71657460d74
yachea <dev.yachea@gmail.com>
2025-07-07 11:11:17 +0900

    [docs] 비동기 통신의 이해

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-class MathTool {
-  static PI: number = 3.14;
-  static muliti(x: number, y: number) {
-    return x * y;
+type Method = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+async function getData<T>(
+  addr: string,
+  method: Method
+): Promise<T | undefined> {
+  const url: string = `https://jsonplaceholder.typicode.com/${addr}`;
+  try {
+    const response: Response = await fetch(url, { method });
+    if (response.ok) {
+      const result: T = await response.json();
+      return result;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
   }
 }
-MathTool.PI;
-MathTool.muliti(5, 6);
+// 전체 POSTS 글 가져오기
+type PostType = { userId: number; id: number; title: string; body: string };
+type AlbumType = { userId: number; id: number; title: string };
+
+async function getPosts() {
+  try {
+    const res = await getData<PostType[]>("posts", "GET");
+    const res2 = await getData<AlbumType[]>("albuns", "GET");
+  } catch (error) {
+    console.log(`${error}가 발생하였습니다.`);
+  }
+}
