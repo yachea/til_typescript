4cb8c5ef5b9cd6d96ff9ffec765fdab45d00a203
yachea <dev.yachea@gmail.com>
2025-07-15 10:18:33 +0900

    [docs] 자료형 정리

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-function getScore(subject: string, score: number) {
-  console.log(`${subject}의 점수는 ${score} 입니다.`);
-}
-/**
- * Parameters 은 함수의 매개변수 데이터 종류 추출
- *  [subject: string, score: number]
- */
+type IdolType = {
+  name: string;
+  age: number;
+  year: number;
+};
 
-type ScoreType = Parameters<typeof getScore>;
+let bts: IdolType = {
+  name: "BTS",
+  age: 20,
+  year: 2020,
+};
