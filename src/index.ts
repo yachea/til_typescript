db35ffcffd0d6c7a3c39fa517e9c511f251c279d
yachea <dev.yachea@gmail.com>
2025-07-28 10:03:38 +0900

    [docs] 클래스에서의 제네릭 정리

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-class NumberPagination<T, K> {
-  // 필수속성
-  data: T[] = [];
-  message?: K;
-  lastFetchAt?: Date;
-  // new 하면 실행될 생성자 함수
-  constructor(data: T[], message?: K, lastFetchAt?: Date) {
-    this.data = data;
-    this.message = message;
-    this.lastFetchAt = lastFetchAt;
-  }
-}
+// fetch 또는 axios 를 이용해서 데이터를 연동시 엄청 자주 활용함.
+const afterTwoSeconds = function (): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // 성공시 실행할 함수
+      resolve("성공이므로 실행함");
+    }, 2000);
+  });
+};
 
-const a = new NumberPagination<string, number>(["아이유", "지민"], 2025);
+const runner = async () => {
+  const res = await afterTwoSeconds();
+  console.log(res);
+};
+
+runner();
