1486502a14e27a155e1fe3914b43d0a492b076c0
yachea <dev.yachea@gmail.com>
2025-07-29 10:18:28 +0900

    [docs] 유틸리티 타입의 이해

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-// fetch 또는 axios 를 이용해서 데이터를 연동시 엄청 자주 활용함.
-const afterTwoSeconds = function (): Promise<string> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // 성공시 실행할 함수
-      resolve("성공이므로 실행함");
-    }, 2000);
-  });
-};
+type IU = "Iue";
+type Upper = Uppercase<IU>;
+// type Upper = "IUE"
 
-const runner = async () => {
-  const res = await afterTwoSeconds();
-  console.log(res);
-};
+type Lower = Lowercase<IU>;
+// type Lower = "iue"
 
-runner();
+type Capital = Capitalize<IU>;
+// type Capital = "Iue"
+
+type UnCaptial = Uncapitalize<IU>;
+// type UnCaptial = "iue"
